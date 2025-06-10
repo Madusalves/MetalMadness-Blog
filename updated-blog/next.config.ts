b24dc1610ad10ea1689711f84f995d98b30dee03
Madusalves <malvesduarte444@gmail.com>
2025-06-10 11:44:34 -0300

    feat: update image domains and add remote patterns in next.config.ts

--- updated-blog/next.config.ts
+++ updated-blog/next.config.ts
@@ -2,7 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["th.bing.com", "www.laut.de", "i.pinimg.com", "pinterest.com"],
+    domains: ["th.bing.com", "www.laut.de", "i.pinimg.com", "br.pinterest.com"],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'th.bing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.laut.de',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'br.pinterest.com',
+      },
+    ]
   },
 };
 
