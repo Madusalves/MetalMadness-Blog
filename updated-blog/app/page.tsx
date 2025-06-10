67091b4711e80a612785faddb11068d08d88cc02
Madusalves <malvesduarte444@gmail.com>
2025-06-10 09:39:36 -0300

    feat: update Home component to include header and Footer for improved layout

--- updated-blog/app/page.tsx
+++ updated-blog/app/page.tsx
@@ -1,8 +1,14 @@
 
+import Footer from "./components/Footer/footer";
 import "./globals.css";
 
 export default function Home() {
   return (
-   <></>
+   <>
+   <h1>ola</h1>
+   <Footer />
+   
+   
+   </>
   );
 }
